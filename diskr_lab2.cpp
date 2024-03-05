a86c38895afcb0b1f7cff2ef7c679edb30b777e7
1gpm1 <ilovedota59@gmail.com>
2024-03-05 13:40:37 +0200

    Add project files.

---
+++ diskr_lab2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::cout << "Таблиця істинності для (p⨁q)^(p⨁r)(f(n))\n\n";
+    std::cout << "| p | q | r | f(n) |\n";
+    std::cout << "|---|---|---|------|\n";
+
+    for (int p = 0; p <= 1; p++) {
+        for (int q = 0; q <= 1; q++) {
+            for (int r = 0; r <= 1; r++) {
+                bool fn = (p != q) && (p != r);
+                std::cout << "| " << p << " | " << q << " | " << r << " |  " << fn << "  |\n";
+            }
+        }
+    }
+
+    return 0;
+}
